6030a9fade0feb4478127510c4e73a60326b3ed9
Moosan <washer2016@outlook.jp>
2020-03-06 02:15:46 +0900

    AddPrefabSystem

---
+++ Assets/Scripts/General/PrefabSystem/BasePrefab.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+namespace General.PrefabSystem
+{
+    public abstract class BasePrefab<TParam> : MonoBehaviour where TParam : BasePrefabParam
+    {
+        private bool Initialized { get; set; } = false;
+        protected TParam MyParam;
+        public void Initialize(TParam param)
+        {
+            MyParam = param;
+            Initialized = true;
+        }
+        protected virtual void Start()
+        {
+            if (!Initialized)
+            {
+                Debug.LogError("PrefabがInitializeされていません");
+                return;
+            }
+        }
+    }
+
+
+    public abstract class BasePrefabParam
+    {
+
+    }
+}
---
+++ Assets/Scripts/General/PrefabSystem/BasePrefabFactory.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+namespace General.PrefabSystem
+{
+    public abstract class BasePrefabFactory<TPrefab,TParam>:MonoBehaviour
+        where TParam : BasePrefabParam
+        where TPrefab : BasePrefab<TParam>
+    {
+        [SerializeField]
+        private GameObject prefabGameObject;
+        protected virtual void Awake()
+        {
+            if(prefabGameObject == null)
+            {
+                Debug.LogError("FactoryError:"+ this.GetType() + "に" + nameof(TPrefab) +"が設定されていません");
+            }
+        }
+        public TPrefab Create(TParam param,Vector3 pos,Quaternion rot,Transform parent = null)
+        {
+            var prefab = Instantiate(prefabGameObject,pos,rot).GetComponent<TPrefab>();
+            if(parent != null)
+            {
+                prefab.transform.parent = parent;
+            }
+            if(prefab == null)
+            {
+                Debug.LogError("FactoryError:PrefabGameObjectに" + nameof(TPrefab) + "がアタッチされていません");
+            }
+            if(param == null)
+            {
+                Debug.LogError("FactoryError:" + nameof(TParam) + "がNullです");
+            }
+            prefab.Initialize(param);
+            return prefab;
+        }
+        public TPrefab Create(TParam param)
+        {
+            return Create(param,Vector3.zero);
+        }
+        public TPrefab Create(TParam param,Vector3 pos)
+        {
+            return Create(param, pos, Quaternion.identity);
+        }
+
+    }
+}
